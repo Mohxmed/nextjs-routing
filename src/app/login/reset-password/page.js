a52af0019d5764a96f7f37c71084afb4edff857c
Mohamed Amr <MohxmedAmr@gmail.com>
2026-07-22 14:37:56 +0300

    Fix Reset-Password Link

--- src/app/login/reset-password/page.js
+++ src/app/login/reset-password/page.js
@@ -5,7 +5,8 @@ import React from 'react'
 function Reset() {
   return (
     <>
-    <h1 className="text-6xl mb-10">تسجيل الدخول/استعادة كلمة المرور</h1>
+    <p>الرئيسية / تسجيل الدخول /<span className='text-pink-500'>نسيت كلمة المرور</span></p>
+    <h1 className="text-6xl mb-10">استعادة كلمة المرور</h1>
     <div className={boxStyles.placeholder}>صفحة نموذج لإستعادة كلمة المرور</div>
      <Link className={buttonStyles.primary} href='/'>العودة للصفحة الرئيسية</Link>
     </>
