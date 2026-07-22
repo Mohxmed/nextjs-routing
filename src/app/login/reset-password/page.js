b3b9e3e091131ccff5f215eea2adb23ed944b4b5
Mohamed Amr <MohxmedAmr@gmail.com>
2026-07-22 14:49:47 +0300

    update button styles

--- src/app/login/reset-password/page.js
+++ src/app/login/reset-password/page.js
@@ -5,7 +5,7 @@ import React from 'react'
 function Reset() {
   return (
     <>
-    <p>الرئيسية / تسجيل الدخول /<span className='text-pink-500'>نسيت كلمة المرور</span></p>
+    <p>الرئيسية / الدخول / <span className='text-pink-500'>نسيت كلمة المرور</span></p>
     <h1 className="text-6xl mb-10">استعادة كلمة المرور</h1>
     <div className={boxStyles.placeholder}>صفحة نموذج لإستعادة كلمة المرور</div>
      <Link className={buttonStyles.primary} href='/'>العودة للصفحة الرئيسية</Link>
--- src/lib/styles.js
+++ src/lib/styles.js
@@ -1,7 +1,6 @@
 export const buttonStyles = {
   primary:
-    "px-6 py-2 rounded-sm bg-blue-600 hover:bg-pink-400 transition-all text-white",
-  secondary:
+"px-6 py-2 rounded-sm bg-gradient-to-r from-blue-600 to-blue-900 bg-[length:200%_100%] bg-left hover:bg-right transition-all duration-200 text-white shadow-lg hover:shadow-blue-500/50",  secondary:
     "px-6 py-2 rounded-sm bg-gray-600 hover:bg-gray-700 transition-all text-white",
   danger:
     "px-6 py-2 rounded-sm bg-red-600 hover:bg-red-700 transition-all text-white",
